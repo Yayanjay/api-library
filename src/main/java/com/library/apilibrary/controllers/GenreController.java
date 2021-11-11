54fec8d2cf1b82d79840c2eb12da9df416b5d786
Yayanjay <yayanabdillah01@gmail.com>
2021-11-11 13:14:19 +0700

    final

--- src/main/java/com/library/apilibrary/controllers/GenreController.java
+++ src/main/java/com/library/apilibrary/controllers/GenreController.java
@@ -6,6 +6,7 @@ import com.library.apilibrary.service.GenreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/genre")
+@CrossOrigin(origins = "http://localhost:8080")
 public class GenreController {
     
     @Autowired
